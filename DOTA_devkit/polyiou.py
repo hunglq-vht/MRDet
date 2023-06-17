38ef5370e06373158bffba5bf7a366ba4573b0e4
HLQ <lequanghung98@gmail.com>
2023-06-17 22:17:41 +0700

    change module name

--- DOTA_devkit/polyiou.py
+++ DOTA_devkit/polyiou.py
@@ -19,15 +19,15 @@ if _swig_python_version_info >= (2, 7, 0):
 elif _swig_python_version_info >= (2, 6, 0):
     def swig_import_helper():
         from os.path import dirname
-        import imp
+        import importlib
         fp = None
         try:
-            fp, pathname, description = imp.find_module('_polyiou.cpython-37m-x86_64-linux-gnu.so', [dirname(__file__)])
+            fp, pathname, description = importlib.find_module('_polyiou.cpython-37m-x86_64-linux-gnu.so', [dirname(__file__)])
         except ImportError:
             import _polyiou
             return _polyiou
         try:
-            _mod = imp.load_module('_polyiou', fp, pathname, description)
+            _mod = importlib.load_module('_polyiou.cpython-37m-x86_64-linux-gnu.so', fp, pathname, description)
         finally:
             if fp is not None:
                 fp.close()
